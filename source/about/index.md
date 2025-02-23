794e11d9ae6dec4f43424fab131123b0ba860895
Crazy_harp <av3709035@163.com>
2025-02-23 18:34:55 +0800

    更新一篇观后感&&修改布局

--- source/about/index.md
+++ source/about/index.md
@@ -3,13 +3,17 @@ title: about
 layout: about
 ---
 # Crazyharp
+
+
 <p style="color: green;font-size: larger">Accepted！ </p>
 
 # 目前进度
 * 搬迁算法模板到网站上
-* 更改布局
-* 这个B日历怎么不记录啊（恼
-* Last Update: 2025-1-21
+* 日历功能已经修复
+* Last Update: 2025-1-31
+
+
+# 更新月历
 {% calendar %}
 {"monthLang": "cn", "dayLang": "cn", "title": "更新日历"}
 {% endcalendar %}
--- source/js/paiban.js
+++ source/js/paiban.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
             Array.from(element.childNodes).forEach(childNode => {
                 if (childNode.nodeType === Node.ELEMENT_NODE && childNode.tagName.toLowerCase() === 'br') {
                     // 创建一个文本节点，包含十个 nbsp
-                    const nbspText = document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0');
+                    const nbspText = document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0');
                     // 在 br 标签后插入文本节点
                     childNode.parentNode.insertBefore(nbspText, childNode.nextSibling);
                 } else if (childNode.nodeType === Node.TEXT_NODE || childNode.nodeType === Node.ELEMENT_NODE) {
@@ -21,10 +21,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             });
         }
-
+        function addNbspAtStart(element) {
+            // 创建一个文本节点，包含十个 nbsp
+            const nbspText = document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0');
+            // 在 p 元素的最前插入文本节点
+            element.insertBefore(nbspText, element.firstChild);
+        }
         // 遍历每个 p 元素，并应用函数
         Array.from(paragraphs).forEach(paragraph => {
             addNbspAfterBr(paragraph);
+            addNbspAtStart(paragraph);
         });
     }
 });
